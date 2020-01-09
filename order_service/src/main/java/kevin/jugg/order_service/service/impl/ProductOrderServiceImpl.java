e0e0de23289b82daabf98bf2e1b3035a2e1cde8b
kevin <hewenkai_1992@sina.com>
2020-01-09 18:26:24 +0800

    ribbon

--- order_service/src/main/java/kevin/jugg/order_service/service/impl/ProductOrderServiceImpl.java
+++ order_service/src/main/java/kevin/jugg/order_service/service/impl/ProductOrderServiceImpl.java
@@ -2,6 +2,8 @@ package kevin.jugg.order_service.service.impl;
 
 import kevin.jugg.order_service.domain.ProductOrder;
 import kevin.jugg.order_service.service.ProductOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
 import java.util.UUID;
@@ -12,10 +14,13 @@ import java.util.UUID;
  * @Create 2020-01-09 18:18
  */
 public class ProductOrderServiceImpl implements ProductOrderService {
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Override
     public ProductOrder save(int userId, int productId) {
         //获取商品详情 dubbo是调用方法 普通http调用用httpclient 这里用ribbon
-
+        restTemplate.getForObject("", Object.class);
 
         ProductOrder productOrder = new ProductOrder();
         productOrder.setCreateTime(new Date());
